93f0fc46c87cd6ed730f2013ecbe1026aae97ac7
lucasdxl <lukvilar.lv@gmail.com>
2026-03-25 20:57:21 -0300

    gererAssembly-> Funções
    Criação das funções básicas para gerar o arquivo de saída .s em Assembly

--- src/gerarAssembly.py
+++ src/gerarAssembly.py
@@ -1,1 +1,22 @@
+def ValidarNumero(token):
+    try:
+        float(token) # verifica se é numero
+        return True # retorna verdadeiro se for
+    except ValueError:
+        return False # retorna falso se não for numero
 
+def gerarAssembly(tokens, arquivoSaida = 'out.s'):
+    assembly = []
+    numeros = []
+    contadorNum = [0]
+
+    k = 0
+    # percorrer lista de tokens
+    while k < len(tokens):
+        token = tokens[k]
+
+    # verifica se for um numero
+    if ValidarNumero(token):
+        marca = f"n{contadorNum[0]}"
+        contadorNum[0] += 1 # aumenta contador de numeros
+        numeros.append((marca, token)) # salva os numeros na lista de numeros
