97bd58c55153900a9b368a1cb3c949b7bc7fc8d6
victorrahal <victorrahal54@gmail.com>
2026-03-25 20:37:46 -0300

    vr/Atualizando lerArquivo
    incrementando variável linha vazia e erro

--- src/lerArquivo.py
+++ src/lerArquivo.py
@@ -4,14 +4,18 @@ def lerArquivo(nomeArquivo):
     linhas = []
 
     try:
-        with open(nomeArquivo, 'r') as f:
-            for linha in f:
+        with open(nomeArquivo, 'r') as arquivo:
+            for linha in arquivo:
                 linha = linha.strip()
-                if linha:
+                if linha != "":
                     linhas.append(linha)
 
     except FileNotFoundError:
         print(f"Erro: arquivo '{nomeArquivo}' não encontrado.")
         return []
+    
+    except Exception as erro:
+        print(f"Erro ao ler o arquivo '{nomeArquivo}': {erro}")
+        return[]
 
     return linhas
