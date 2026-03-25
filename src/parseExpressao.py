d5e1e747cdb939c1f639c0173408c0e45316a66e
Jhtomaz <joaohenriquet.d@gmail.com>
2026-03-25 18:33:57 -0300

    Update parseExpressao.py
    Comitt da função estadoNumero, fazendo um teste com um contador como ponto, lógica do número como lista também implantada.

--- src/parseExpressao.py
+++ src/parseExpressao.py
@@ -1,1 +1,11 @@
-#Teste
+def estadoNumero(linha):
+  ponto = 0
+  numero = []
+  print(linha)
+  for i in range(len(linha)):
+    if linha[i] == '.':
+      if ponto == 1:
+        raise Exception("Número inválido")
+      ponto += 1
+    numero.append(linha[i])
+  return "".join(numero)
