6824f993a5734d53c2f0a200f7719f865575cbf7
Jhtomaz <joaohenriquet.d@gmail.com>
2026-03-26 19:34:31 -0300

    Update parseExpressao.py
    Mudança no estadoNumero, estava tendo problemas com a falta de índice antes. No protótipo ele estava puxando números "10" de forma separada

--- src/parseExpressao.py
+++ src/parseExpressao.py
@@ -1,14 +1,21 @@
-def estadoNumero(linha):
-  ponto = 0
-  numero = []
-  print(linha)
-  for i in range(len(linha)):
-    if linha[i] == '.':
-      if ponto == 1:
-        raise Exception("Número inválido")
-      ponto += 1
-    numero.append(linha[i])
-  return "".join(numero)
+def estadoNumero(linha, i):
+    numero = []
+    ponto = 0
+
+    while i < len(linha):
+        num = linha[i]
+        if num.isdigit():
+            numero.append(num)
+        elif num == ".":
+            if ponto > 1:
+                raise Exception("Número inválido, mais de um ponto")
+            ponto = 1
+            numero += num
+        else:
+            break
+        i = i + 1
+    numero = "".join(numero)
+    return numero, i
 
 def estadoOperador(linha):
   for i in range(len(linha)):
