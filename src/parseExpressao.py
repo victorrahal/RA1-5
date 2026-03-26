3512025c65e4eee11e7b459d9535bedbd98bb2ba
victorrahal <victorrahal54@gmail.com>
2026-03-26 20:25:07 -0300

    vr/uptade parseExpressao
    corrige estadoNumero e estadoComando, adiciona estadoIdentificador, estadoInicial e parseExpressao

--- src/parseExpressao.py
+++ src/parseExpressao.py
@@ -7,7 +7,7 @@ def estadoNumero(linha, i):
         if num.isdigit():
             numero.append(num)
         elif num == ".":
-            if ponto > 1:
+            if ponto >= 1:
                 raise Exception("Número inválido, mais de um ponto")
             ponto = 1
             numero += num
@@ -23,14 +23,45 @@ def estadoOperador(linha, i):
     else:
         return linha[i], i+1
   
-def estadoComando(linha):
-  comando = []
-  for i in range(len(linha)):
-    entrada = linha[i]
-    comando.append(entrada)
-  if linha not in ["MEM","RES"]:
-    raise Exception(f"Comando inválido")
-  return linha
+def estadoComando(token):
+    if not token.isalpha() or not token.isupper():
+        raise Exception(f"Comando inválido: {token}")
+    return token
+
+def estadoIdentificador(linha, i):
+    identificador = []
+    while i < len(linha):
+        char = linha[i]
+        if char.isalpha() and char.isupper():
+            identificador.append(char)
+            i += 1
+        else:
+            break
+    token = "".join(identificador)
+    return estadoComando(token), i
 
 def estadoParenteses(linha, i):
     return linha[i], i+1
+
+def estadoInicial(linha, i):
+    char = linha[i]
+    if char.isdigit():
+        return estadoNumero(linha, i)
+    if char.isupper():
+        return estadoIdentificador(linha, i)
+    if char in ["+", "-", "*", "/", "%", "^", "//"]:
+        return estadoOperador(linha, i)
+    if char in ["(", ")"]:
+        return estadoParenteses(linha, i)
+    if char == " ":
+        return None, i + 1
+    raise Exception(f"Caractere inválido: '{char}'")
+
+def parseExpressao(linha):
+    tokens = []
+    i = 0
+    while i < len(linha):
+        token, i = estadoInicial(linha, i)
+        if token is not None:
+            tokens.append(token)
+    return tokens
