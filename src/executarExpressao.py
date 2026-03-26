d09e27d6463945eeaa5415c50eee53c2adb50ffd
victorrahal <victorrahal54@gmail.com>
2026-03-26 20:14:48 -0300

    vr/atualizando executarExpressao
    corrige validar_int para retornar int, buscar_memoria retorna 0.0 e adiciona flattenTokens para aluno 3

--- src/executarExpressao.py
+++ src/executarExpressao.py
@@ -16,12 +16,12 @@ def validar_int(token):
         raise ValueError(f"Valor inválido para RES: {token}")
     valor = float(token)
     if not valor.is_integer():
-        raise ValueError(f"RES não pode ter índice negativo: {token}")
+        raise ValueError(f"RES requer índice não negativo: {token}")
     return valor
 
 def buscar_memoria(nome_mem, memorias):
     if nome_mem not in memorias:
-        raise ValueError(f"Memória não iniciada: {nome_mem}")
+        return 0.0
     return memorias[nome_mem]
 
 def buscar_res_anterior(indice, res_anteriores):
@@ -100,6 +100,9 @@ def avaliar_tokens(tokens_limpos, memorias, res_anteriores):
         raise ValueError("Expressão inválida: pilha inconsistente")
 
     return pilha[0]
+
+def flattenTokens(tokens):
+    return [token for token in tokens if token not in ["(", ")"]]
 
 def executarExpressao (tokens, memorias, res_anteriores):
     pilha = []
